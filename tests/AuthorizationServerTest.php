c1be48eb7b7c09175542ee598fafca7950d5333a
MilesChou <jangconan@gmail.com>
2018-09-14 18:06:37 +0800

    Add Elliptic Curves based algorithms

--- tests/AuthorizationServerTest.php
+++ tests/AuthorizationServerTest.php
@@ -4,7 +4,8 @@ namespace Tests;
 
 use Lcobucci\JWT\Builder;
 use Lcobucci\JWT\Parser;
-use Lcobucci\JWT\Signer\Rsa\Sha256;
+use Lcobucci\JWT\Signer\Rsa;
+use Lcobucci\JWT\Signer\Ecdsa;
 use Lcobucci\JWT\Token;
 use League\OAuth2\Server\AuthorizationServer;
 use League\OAuth2\Server\Repositories\AccessTokenRepositoryInterface;
@@ -20,8 +21,6 @@ use Zend\Diactoros\ServerRequestFactory;
 
 class AuthorizationServerTest extends TestCase
 {
-    const DEFAULT_SCOPE = 'basic';
-
     public function setUp()
     {
         // Make sure the keys have the correct permissions.
@@ -63,7 +62,7 @@ class AuthorizationServerTest extends TestCase
             ->setNotBefore(time() + 60)
             ->setExpiration(time() + 3600)
             ->set('uid', 1)
-            ->sign(new Sha256(), file_get_contents(__DIR__ . '/Stubs/private.key'))
+            ->sign(new Rsa\Sha256(), file_get_contents(__DIR__ . '/Stubs/private.key'))
             ->getToken();
 
         $response = $server->respondToAccessTokenRequest(ServerRequestFactory::fromGlobals(), new Response);
@@ -73,6 +72,51 @@ class AuthorizationServerTest extends TestCase
         $jwt = (new Parser)->parse($json['access_token']);
 
         $this->assertInstanceOf(Token::class, $jwt);
+    }
 
+    public function testValidateJwtBearerGrantUsingAnotherAlg()
+    {
+        $clientEntity = new ClientEntity();
+        $clientRepository = $this->getMockBuilder(ClientRepositoryInterface::class)->getMock();
+        $clientRepository->method('getClientEntity')->willReturn($clientEntity);
+
+        $scope = new ScopeEntity();
+        $scopeRepositoryMock = $this->getMockBuilder(ScopeRepositoryInterface::class)->getMock();
+        $scopeRepositoryMock->method('getScopeEntityByIdentifier')->willReturn($scope);
+        $scopeRepositoryMock->method('finalizeScopes')->willReturnArgument(0);
+
+
+        $accessTokenRepositoryMock = $this->getMockBuilder(AccessTokenRepositoryInterface::class)->getMock();
+        $accessTokenRepositoryMock->method('getNewToken')->willReturn(new AccessTokenEntity());
+
+        $server = new AuthorizationServer(
+            $clientRepository,
+            $accessTokenRepositoryMock,
+            $scopeRepositoryMock,
+            'file://' . __DIR__ . '/Stubs/private.key',
+            'file://' . __DIR__ . '/Stubs/public.key'
+        );
+
+        $server->enableGrantType(new JwtBearerGrant('file://' . __DIR__ . '/Stubs/es256-public.key'));
+
+        $_POST['grant_type'] = 'urn:ietf:params:oauth:grant-type:jwt-bearer';
+        $_POST['assertion'] = (string)(new Builder)
+            ->setIssuer('http://example.com')
+            ->setAudience('http://example.org')
+            ->setId('4f1g23a12aa')
+            ->setIssuedAt(time())
+            ->setNotBefore(time() + 60)
+            ->setExpiration(time() + 3600)
+            ->set('uid', 1)
+            ->sign(new Ecdsa\Sha384(), file_get_contents(__DIR__ . '/Stubs/es256-private.key'))
+            ->getToken();
+
+        $response = $server->respondToAccessTokenRequest(ServerRequestFactory::fromGlobals(), new Response);
+
+
+        $json = json_decode($response->getBody(), true);
+        $jwt = (new Parser)->parse($json['access_token']);
+
+        $this->assertInstanceOf(Token::class, $jwt);
     }
 }
